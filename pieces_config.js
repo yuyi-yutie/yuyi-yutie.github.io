c3743b0557cfbf6571f88b3ee95108a2a07f4e00
yu <yy1973100@163.com>
2026-04-16 14:15:39 +0800

    update csc.html

---
+++ pieces_config.js
@@ -0,0 +1,15 @@
+// 棋子配置文件
+var piecesConfig = {
+  "pieces": [
+    {"id": 0, "name": "e_1", "color": "red"},
+    {"id": 1, "name": "e_2", "color": "red"},
+    {"id": 2, "name": "e_3", "color": "red"},
+    {"id": 3, "name": "e_4", "color": "red"},
+    {"id": 4, "name": "e_5", "color": "red"},
+    {"id": 5, "name": "f_1", "color": "green"},
+    {"id": 6, "name": "f_2", "color": "green"},
+    {"id": 7, "name": "f_3", "color": "green"},
+    {"id": 8, "name": "f_4", "color": "green"},
+    {"id": 9, "name": "f_5", "color": "green"}
+  ]
+};
